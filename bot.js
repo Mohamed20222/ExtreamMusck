948b3e123725a79945db98b369af8004d38184a3
Mohamed20222 <44948619+Mohamed20222@users.noreply.github.com>
2019-01-22 11:23:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Zplay | ExtreamsNetwork`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Zplay | ExtreamesNetwork`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
